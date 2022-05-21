71dc5ecb567c81af2f14b5180ead7f0f7a5a0193
Alexander Loshakov <alevas556@icloud.com>
2022-05-21 11:51:49 +0300

    #1: added location model and viewmodel

---
+++ FavoritePlaces/Sources/Modules/Locations/Model/LocationModel.swift
@@ -0,0 +1,31 @@
+//
+//  Created by Alexander Loshakov on 20/05/2022
+//  Copyright © 2022 Alexander Loshakov. All rights reserved.
+//
+
+import Foundation
+
+struct LocationModel: UniqueIdentifiable, Decodable {
+    let uid: UniqueIdentifier
+    let name: String
+    let state: String
+    let park: String
+    let coordinates: GeoPosition
+    let imageName: String
+    let isFavorite: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case uid = "id"
+        case name
+        case state
+        case park
+        case coordinates
+        case imageName
+        case isFavorite
+    }
+}
+
+struct GeoPosition: Decodable {
+    let longitude: Double
+    let latitude: Double
+}
---
+++ FavoritePlaces/Sources/Modules/Locations/Model/LocationViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Alexander Loshakov on 20/05/2022
+//  Copyright © 2022 Alexander Loshakov. All rights reserved.
+//
+
+import UIKit
+
+struct LocationViewModel {
+    let uid: UniqueIdentifier
+    let title: String
+    let image: UIImage?
+    let isFavorite: Bool
+}
